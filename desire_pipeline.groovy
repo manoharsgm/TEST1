a639413122ee8f1a8ee3617dc64fb2a58a9564eb
manoharsgm <37188387+manoharsgm@users.noreply.github.com>
2020-07-13 17:03:49 +0530

    Update desire_pipeline.groovy

--- desire_pipeline.groovy
+++ desire_pipeline.groovy
@@ -1,30 +1,34 @@
-pipeline {
-    agent any
+pipelineJob('DSL_Demo') {
 
-    stages {
-        stage('cloning repo ... ') {
-            steps {
-                git 'https://github.com/alicommit-malp/jenkins_dsl_example'
-            }
-        }
-        
-        stage('build'){
-            agent{
-                docker { 
-                    image 'python:2' 
-                    reuseNode true
-                }    
-            }
-            steps{
-                sh "pip install -r $WORKSPACE/python/requirements.txt"
-                sh "python $WORKSPACE/python/main.py"
-            }
-        }
-            
+pipeline {
+  agent {
+     node { 
+        label 'BLRCSLFBECM0013'
+        } 
+  }
+  stages {
+    stage('Checkout Scm') {
+      steps {
+        git(branch: '+refs/heads/develop:refs/remotes/origin/develop', credentialsId: 'SSH_cmreub', url: 'ssh://git@blrvslalm0006:7999/es/partyrepos.git')
+      }
     }
-    post{
-        success{
-            echo "Cool :)"
-        }
+
+    stage('No Converter-0') {
+      steps {
+        echo 'No converter for Builder: hudson.tasks.Ant'
+      }
     }
+
+  }
+  options {
+    buildDiscarder(logRotator(numToKeepStr: '5'))
+  }
+  parameters {
+    string(name: 'version', defaultValue: '2020.X', description: '')
+    string(name: 'jdk.home', defaultValue: '/usr', description: '')
+  }
+  triggers {
+    pollSCM('* * * * * ')
+  }
+}
 }
